6dbca572c620353a6ff3be01ba088be437fb57b3
Rodrigo Latorraca <rlatorraca@gmail.com>
2022-11-10 15:07:16 -0400

    Add micro services standards

---
+++ travel-agency-cli/src/main/java/ca/com/rlsp/client/ClientService.java
@@ -0,0 +1,33 @@
+package ca.com.rlsp.client;
+
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.temporal.ChronoUnit;
+
+@RegisterRestClient(baseUri = "http://localhost:8080/client")
+public interface ClientService {
+
+    @POST
+    @Path("newClient")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newClient(Client client);
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("findById")
+    @Timeout(unit = ChronoUnit.SECONDS, value = 3)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+            requestVolumeThreshold = 4,
+            failureRatio = .5,
+            delay = 6000,
+            successThreshold = 1
+    )
+    public Client findById(@QueryParam("id") long id);
+
+}
